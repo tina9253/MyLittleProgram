04e0d5169d6c415369460e394f3d4b9c1ffd6a89
Tina Liu <katrina9253@gmail.com>
2018-01-08 10:17:40 -0600

    debug

--- utils.py
+++ utils.py
@@ -8,15 +8,16 @@ pivot_index = ['MB3G','MB3G_DESCRIPTION']
 """
 
 
-class Helper(object):
+class SasReaderPivot(object):
     """docstring for GeneratePivotTable."""
-
+    pd = __import__('pandas')
+    np = __import__('numpy')
     def __init__(self, df_reader, bins):
         """
 
         :type bins: a list specifying bins to group numerical variables
         """
-        super(Helper, self).__init__()
+        #super(SasReaderPivot, self).__init__()
         self.df_reader = df_reader
         self.bins = bins
 
@@ -25,14 +26,10 @@ class Helper(object):
                   pivot_func_dict,
                   data_cols,
                   filters=None):
-        ret = pd.DataFrame()
+        ret = self.pd.DataFrame()
         for chunk in self.df_reader:
-            # if max_chunk_num > 0 and index > max_chunk_num:
-            #     print ("Exceed max chunk number.")
-            #     break
             piv = self._do_generate_pivot(chunk, pivot_index, pivot_func_dict, filters=filters)
             ret = self._merge_pivot_table(ret, piv, data_cols)
-            print(ret)
         return ret
 
     def _do_generate_pivot(self, data, pivot_index, pivot_func_dict, filters=None):
@@ -41,8 +38,7 @@ class Helper(object):
             raise Exception('Wrong colnames in index')
         data = self._apply_filters(data, filters=filters)
         index, pivot_index = self._generate_category(data, pivot_index)
-        print(pivot_index)
-        return pd.pivot_table(data=data,
+        return self.pd.pivot_table(data=data,
                             index=pivot_index,
                             values=pivot_func_dict.keys(),
                             aggfunc=pivot_func_dict,
@@ -54,7 +50,7 @@ class Helper(object):
             return data
         if not set(list(filters.keys())).issubset(set(data.columns)):
             raise Exception('Wrong colnames in filters')
-        filter_now = pd.Series()
+        filter_now = self.pd.Series()
         for key, value in filters.items():
             filter_now &=  [data.loc[row,key] in value for row in data.index]
             filter_index = data.index[filter_now]
@@ -69,19 +65,19 @@ class Helper(object):
                 continue
             new_name = column + '_grp'
             try:
-                data[new_name] = pd.cut(data[column], self.bins, right=False)
+                data[new_name] = self.pd.cut(data[column], self.bins, right=False)
             except:
                 raise Exception('Cannot find bins. Please specify.')
             index.append(new_name)
             pivot_index_mod.append(new_name)
         return index, pivot_index_mod
 
-    def _merge_pivot_table(original, pivot, data_cols):
-        new_pivot = pd.concat([original, pivot], axis = 1).fillna(0)
+    def _merge_pivot_table(self, original, pivot, data_cols):
+        new_pivot = self.pd.concat([original, pivot], axis = 1).fillna(0)
         previous = new_pivot.iloc[:, 0:len(data_cols)]
         current = new_pivot.iloc[:, len(data_cols):]
         if current.shape[1]:
-            new_pivot_array = np.add(np.array(previous), np.array(current))
+            new_pivot_array = self.np.add(self.np.array(previous), self.np.array(current))
         else:
-            new_pivot_array = np.array(previous)
-        return pd.DataFrame(new_pivot_array, index = new_pivot.index)
+            new_pivot_array = self.np.array(previous)
+        return self.pd.DataFrame(new_pivot_array, index = new_pivot.index)
